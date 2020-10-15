3c0fc53e4299048c88d7527e76cdbf03e45f23cb
Sergey Illarionov <Sergey-ill@mail.ru>
2020-10-15 21:04:06 +0300

    addNewWebSite

---
+++ main.js
@@ -0,0 +1,22 @@
+﻿function loadMoreHandler() {
+    const url = loadMoreBtn.dataset.more
+
+        const xhr = new XMLHttpRequest() 
+        xhr.open('GET', url)
+        xhr.send()
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState === 4) {
+                if(xhr.status === 200) {
+                    loadMoreBtn.removeEventListener('click', loadMoreHandler)
+                    loadMoreBtn.remove()
+                    cardList.insertAdjacentHTML('beforeend', xhr.responseText)
+                    initLoadBtn()
+                } else {
+                    alert("Ошибка" + xhr.statusText)
+                }
+            }
+        }
+
+}
+
+// новый коменнтарий
